c49b8ecffa09a9b565ae797cc2a197ca7a94b83b
Pedro Luiz <p3dr0c4@live.com>
2020-08-04 10:36:41 -0300

    Modificações Área do Usuário + Lint

--- src/views/AreaDoUsuario.vue
+++ src/views/AreaDoUsuario.vue
@@ -4,7 +4,13 @@
     <v-container class="usuarioContainer">
       <v-form ref="form">
         <v-row>
-          <v-col cols="12" lg="6">
+          <v-col cols="12" lg="4">
+            <v-img
+              src="http://placehold.it/100x100?text=Foto"
+              class="width"
+            ></v-img>
+          </v-col>
+          <v-col cols="12" lg="8">
             <v-text-field
               v-model="usuario.name"
               label="Nome"
@@ -16,10 +22,44 @@
               required
             ></v-text-field>
           </v-col>
-          <v-col cols="12" lg="6">
-            <h2 class="text-center">Integrações</h2>
+          <v-col cols="6">
+            <v-btn color="purple white--text" width="100%"
+              >WCA Autenticado</v-btn
+            >
+          </v-col>
+          <v-col cols="6">
+            <v-btn color="disabled" width="100%">E-mail confirmado</v-btn>
           </v-col>
         </v-row>
+        <v-progress-linear
+          :active="!usuario.wca"
+          indeterminate
+          color="purple"
+        ></v-progress-linear>
+        <v-col cols="12" v-if="usuario.wca">
+          <v-list>
+            <v-card class="mx-auto" max-width="100%" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-4">
+                    <a :href="usuario.wca.person.url">Visitar Perfil</a>
+                  </div>
+                  <v-list-item-title class="headline mb-1">{{
+                    usuario.wca.person.name
+                  }}</v-list-item-title>
+                  <v-list-item-subtitle
+                    >País:
+                    {{ usuario.wca.person.country_iso2 }}</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+
+                <v-list-item-avatar tile size="100" color="gray">
+                  <v-img :src="usuario.wca.person.avatar.thumb_url"></v-img>
+                </v-list-item-avatar>
+              </v-list-item>
+            </v-card>
+          </v-list>
+        </v-col>
       </v-form>
     </v-container>
   </div>
@@ -32,15 +72,29 @@ export default {
     return {
       usuario: {
         name: "",
-        email: ""
+        email: "",
+        wca: null
       }
     };
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    async fetchUser() {
+      let response = await fetch(
+        "https://www.worldcubeassociation.org/api/v0/persons/2017PIRE01"
+      );
+      let data = await response.json();
+      console.log(data);
+      this.usuario.wca = data;
+    }
   }
 };
 </script>
 
 <style scoped>
 .usuarioContainer {
-  width: 600px;
+  max-width: 600px;
 }
 </style>
